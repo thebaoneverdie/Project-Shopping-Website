14bb7991cc5bcb6fbcac48941aa044c924eda9aa
the bao <boykg1900@gmail.com>
2024-10-31 11:59:07 +0700

    Add project files.

---
+++ WebBanHangOnline/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+		ApplicationDbContext _dbConnect = new ApplicationDbContext();
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            var items = _dbConnect.Categories;
+            return View(items);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Category model)
+        {
+            if(ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                _dbConnect.Categories.Add(model);
+                _dbConnect.SaveChanges();
+				return RedirectToAction("Index");
+			}
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+			var item = _dbConnect.Categories.Find(id);
+			if (item == null)
+			{
+				return HttpNotFound();
+			}
+			return View(item); // Truyền đối tượng item vào View
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(Category model)
+		{
+			if (ModelState.IsValid)
+			{
+                _dbConnect.Categories.Attach(model);
+				model.ModifiedDate = DateTime.Now;
+				model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+
+                _dbConnect.Entry(model).Property(x => x.Title).IsModified = true;
+				_dbConnect.Entry(model).Property(x => x.Description).IsModified = true;
+				_dbConnect.Entry(model).Property(x => x.Alias).IsModified = true;
+				_dbConnect.Entry(model).Property(x => x.SeoDescription).IsModified = true;
+				_dbConnect.Entry(model).Property(x => x.SeoKeywords).IsModified = true;
+				_dbConnect.Entry(model).Property(x => x.SeoTitle).IsModified = true;
+				_dbConnect.Entry(model).Property(x => x.Position).IsModified = true;
+				_dbConnect.Entry(model).Property(x => x.ModifiedDate).IsModified = true;
+				_dbConnect.Entry(model).Property(x => x.ModifiedBy).IsModified = true;
+
+				_dbConnect.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(model);
+		}
+
+		public ActionResult Delete(int id)
+		{
+			var item = _dbConnect.Categories.Find(id);
+			if(item != null)
+			{
+				_dbConnect.Categories.Remove(item);
+				_dbConnect.SaveChanges();
+				return Json(new { success = true });
+			}
+			return Json(new { success = false });
+		}
+	}
+}
---
+++ WebBanHangOnline/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        private ApplicationDbContext _dbContext = new ApplicationDbContext();
+        // GET: Admin/News
+        public ActionResult Index()
+        {
+            var items = _dbContext.News.OrderByDescending(x => x.Id).ToList();
+            return View(items);
+        }
+
+        public ActionResult Add()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(News model)
+        {
+            if(ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                model.CategoryId = 3;
+                model.Alias  = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Alias);
+                _dbContext.News.Add(model);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+
+        }
+		public ActionResult Edit(int id)
+		{
+            var item = _dbContext.News.Find(id);
+			return View(item);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(News model)
+		{
+			if (ModelState.IsValid)
+			{
+				model.ModifiedDate = DateTime.Now;
+				model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Alias);
+				_dbContext.News.Attach(model);
+                _dbContext.Entry(model).State = System.Data.Entity.EntityState.Modified;
+				_dbContext.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(model);
+
+		}
+		[HttpPost]
+		public ActionResult Delete(int id)
+        {
+            var item = _dbContext.News.Find(id);
+            if(item != null)
+            {
+                _dbContext.News.Remove(item);
+                _dbContext.SaveChanges();
+				return Json(new { success = true });
+			}
+			return Json(new { success = true });
+		}
+		[HttpPost]
+		public ActionResult IsActive(int id)
+		{
+			var item = _dbContext.News.Find(id);
+			if (item != null)
+			{
+				item.IsActive = !item.IsActive;
+				_dbContext.Entry(item).State = System.Data.Entity.EntityState.Modified;
+				_dbContext.SaveChanges();
+				return Json(new { success = true, isActive = item.IsActive });
+			}
+			return Json(new { success = true });
+		}
+	}
+}
