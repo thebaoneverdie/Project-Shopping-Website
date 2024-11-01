37ffbd2652fb656dce163c8a81813d75eb6bda01
the bao <boykg1900@gmail.com>
2024-11-01 14:56:26 +0700

    Adding CRUD function to Posts model

---
+++ WebBanHangOnline/Migrations/202411010215241_AddingNewPropertyToPosts.cs
@@ -0,0 +1,18 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddingNewPropertyToPosts : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.tb_Posts", "SeoTitle", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.tb_Posts", "SeoTitle");
+        }
+    }
+}
--- WebBanHangOnline/Models/EF/Product.cs
+++ WebBanHangOnline/Models/EF/Product.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace WebBanHangOnline.Models.EF
 {
@@ -17,9 +18,12 @@ namespace WebBanHangOnline.Models.EF
 		[StringLength(150)]
 		public string Title { get; set; }
 		public string Alias { get; set; }
+		[StringLength(50)]
 		public string ProductCode {  get; set; } 
 		public string Description { get; set; }
+		[AllowHtml]
 		public string Detail {  get; set; }
+		[StringLength(250)]
 		public string Image {  get; set; }
 		public decimal Price { get; set; }
 		public decimal PriceSale { get; set; }
@@ -32,8 +36,11 @@ namespace WebBanHangOnline.Models.EF
 
 		public bool IsActive { get; set; }
 		public int ProductCategoryId {  get; set; }
+		[StringLength(250)]
 		public string SeoTitle { get; set; }
+		[StringLength(500)]
 		public string SeoDescription { get; set; }
+		[StringLength(250)]
 		public string SeoKeywords { get; set; }
 		public virtual ProductCategory ProductCategory { get; set; }
 
