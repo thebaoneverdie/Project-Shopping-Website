62ddf918e1a7068f0ecdc87abfd9d4b14162fa5f
the bao <boykg1900@gmail.com>
2025-01-24 11:08:03 +0700

    [ModelShoppingCart] Feat: add model ShoppingCart

---
+++ WebBanHangOnline/Scripts/jsShopping.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('body').on('click', '.btnAddToCard', function (e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        var quantity = 1;
+        var tQuantity = $('#quantity_value').text();
+        if (tQuantity != '') {
+            quantity = parseInt(tQuantity);
+        }
+        alert(id + " " + quantity);
+    });
+});
